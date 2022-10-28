ea4c5ecb79972d9ae4f005c6042ec6cf369fb2f1
RundownRhino <52856631+RundownRhino@users.noreply.github.com>
2022-10-29 01:15:17 +0300

    Allow configuring the log folder via watcher config

---
+++ src/aw-watcher-mpv-sender/config.py
@@ -0,0 +1,16 @@
+from aw_core.config import load_config_toml
+from typing import TypedDict
+
+default_log_folder = "full/path/to/log/folder"
+default_config = f"""
+log_folder = "{default_log_folder}"
+"""
+
+
+class Config(TypedDict):
+    log_folder: str
+
+
+CONFIG: Config = load_config_toml("aw-watcher-mpv", default_config=default_config)  # type:ignore
+if not isinstance(CONFIG, dict):
+    raise ValueError("the configuration TOML wasn't a dictionary - perhaps you used a group?", CONFIG)
--- src/aw-watcher-mpv-sender/main.py
+++ src/aw-watcher-mpv-sender/main.py
@@ -3,12 +3,19 @@ from pathlib import Path
 
 from .sender import Sender
 from aw_client.client import ActivityWatchClient
+from .config import CONFIG, default_log_folder
 
 
 def main():
     parser = argparse.ArgumentParser(description="Starts the sender")
-    parser.add_argument("log_folder", type=Path, help="the folder aw-watcher-mpv-logger writes logs to")
+    parser.add_argument(
+        "--log_folder", type=Path, help="the folder aw-watcher-mpv-logger writes logs to", default=CONFIG["log_folder"]
+    )
     args = parser.parse_args()
+    if args.log_folder == Path(default_log_folder):
+        raise ValueError(
+            "Please provide the log folder to read, either in the watcher's TOML config or as the --log-folder argument!"
+        )
     log_folder: Path = args.log_folder
     if not log_folder.exists():
         try:
